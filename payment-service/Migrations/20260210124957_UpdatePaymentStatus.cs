556dc580160d28be2cbee000550c80445d47ad9b
Inscode <inshafahmedh@gmail.com>
2026-02-11 21:42:20 +0530

    add structured log messages

---
+++ payment-service/Migrations/20260210124957_UpdatePaymentStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace payment_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePaymentStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "payments",
+                newName: "Status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "payments",
+                newName: "status");
+        }
+    }
+}
